748dadc7ede402cf9694d78c7e32d5f4b56fd83e
Dinalys <dinada2003@gmail.com>
2024-07-26 17:27:13 +0100

    feat: ajout d'une app dash our afficher un heatmap dynamiquement et encodage de l'image en base64 pour l'integratio dans l'interface web

---
+++ app/dashheatmap.py
@@ -0,0 +1,45 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from app.dash import clean_data
+import io
+import base64
+from app import app
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.switch_backend('Agg')
+external_stylesheet=['/assets/heatmap.css']
+apl= dash.Dash(__name__, server= app, external_stylesheets=external_stylesheet, routes_pathname_prefix='/heatmap/')
+df= clean_data()
+
+def heatmap(dataframe):
+    if isinstance(dataframe, pd.DataFrame):
+        df_num=dataframe.select_dtypes(include=[int, float])
+        if df_num.empty:
+            return "Pas de données numériques"
+        else:
+            plt.figure(figsize=(18,14))
+            sns.heatmap(df_num.corr(), annot=True, cmap= 'coolwarm')
+            buffer= io.BytesIO()
+            plt.savefig(buffer, format='png')
+            buffer.seek(0)
+            img= base64.b64encode(buffer.getvalue()).decode('utf-8')
+            plt.close()
+            return img
+    else:
+        return " Abscence de données"
+    
+apl.layout= html.Div([
+    html.H1("Heatmap de données"),
+    html.Div(id='figure', children=[html.Img(src='data:image/png;base64,{}'.format(heatmap(df)), className='heatmap')],className='heatmapcontainer'),
+    dcc.Interval(id='Interval', n_intervals=0, interval=5*1000)
+])
+
+@apl.callback(Output(component_id='figure', component_property='children'),
+              Input(component_id='Interval', component_property='n_interval'))
+def updateheatmap(n):
+    df= clean_data()
+    return html.Img(src='data:image/png;base64,{}'.format(heatmap(df)), className='heatmap')
