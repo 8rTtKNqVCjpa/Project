e3195b250cdc2b5ed14227af0ee2c31b81e89962
Dinalys <dinada2003@gmail.com>
2024-07-26 17:20:42 +0100

    Supprimer les fcts des visualisations non interatives

--- app/models.py
+++ app/models.py
@@ -6,29 +6,36 @@ import seaborn as sns
 
 chemin = r"C:\Users\hp\Desktop\Projects\projet\mock_data\csvjson.json"
 
-def load_clean_data():
+def load_data():
     if os.path.exists(chemin):
-        with open(chemin, "r", encoding="UTF-8") as file:
+        with open(chemin, 'r',encoding="UTF-8") as file:
             try:
-                data = json.load(file)
-                df = pd.DataFrame(data)
-                print("Valeurs manquantes:")
-                print(df.isnull().sum())
-                df.fillna("inexistant", inplace=True)
-                print("Valeurs manquantes après nettoyage :")
-                print(df.isnull().sum())
+                data= json.load(file)
+                df= pd.DataFrame(data)
                 return df
             except json.JSONDecodeError:
-                print('Error detected: Invalid JSON format')
+                print("Format json incorrect")
                 return None
             except IOError as e:
-                print(f'Error detected: {e}')
+                print({f'Erreur: e'})
                 return None
     else:
-        return None  # Returning None if file does not exist
+        return "Chemin introuvable"
+    
+def clean_data():
+    df= load_data()
+    print("Les valeurs manquantes avant le nettoyage:")
+    print(df.isnull().sum())
+    col_num= df.select_dtypes(include=["int", "float"]).columns
+    col_nonum= df.select_dtypes(exclude=["int", "float"]).columns
+    df[col_num].fillna(df[col_num].mean())
+    df[col_nonum].fillna("Inexistant")
+    print("Les valeurs manquantes apres le nettoyage:")
+    print(df.isnull().sum())
+    return df
 
 def getorderbyid(order_id):
-    df = load_clean_data()
+    df = clean_data()
     if isinstance(df, pd.DataFrame):
         order = df[df['ORDERNUMBER'] == order_id].to_dict(orient='records')
         if order:
@@ -38,7 +45,7 @@ def getorderbyid(order_id):
     return None
 
 def getorderbystatus(order_status):
-    df = load_clean_data()
+    df = clean_data()
     if isinstance(df, pd.DataFrame):
         orders = df[df['STATUS'] == order_status].to_dict(orient='records')
         if orders:
@@ -46,75 +53,4 @@ def getorderbystatus(order_status):
         else:
             return None
     return None
-
-def datadescription():
-    df = load_clean_data()
-    if isinstance(df, pd.DataFrame):
-        datadescr = df.describe()
-        return datadescr
-    return None
-
-def datacorrelation():
-    df = load_clean_data()
-    if isinstance(df, pd.DataFrame):
-        sns.pairplot(df)
-        plt.title("Pairplot for data analysis")
-        plt.savefig('pairplot.png')
-        plt.close()
-        return "Pairplot for data analysis created"
-    else:
-        return "Error: No data available for correlation analysis"
-    
-def dataheatmap():
-    df=load_clean_data()
-    if isinstance(df, pd.DataFrame):
-        numeric_df = df.select_dtypes(include=[int, float])
-        if(numeric_df.empty):
-            return "No numeric data"
-        sns.heatmap(numeric_df.corr(), annot=True)
-        plt.title("The heatmap of the data")
-        plt.savefig("heatmap.png")
-        plt.close()
-        return "Heatmap of the data"
-    else:
-        return "Error: No data available for heatmap visualization"
-    
-def priceofeachbymsrplmplot():
-    df=load_clean_data()
-    if isinstance(df, pd.DataFrame):
-        sns.lmplot(x="MSRP", y="PRICEEACH", data=df, scatter_kws={'alpha':0.3})
-        plt.title("The lmplot for the price of each by MSRP")
-        plt.savefig("Thelmplot.png")
-        plt.close()
-        return "The lmplot"
-    else:
-        return "Error: No data available for the Lmplot visualization"
-    
-def salesbycountryvisualisation():
-    df = load_clean_data()
-    if isinstance(df, pd.DataFrame):
-        plt.figure()
-        df.groupby("COUNTRY")["SALES"].sum().plot(kind='bar')
-        plt.xlabel("Country")
-        plt.ylabel("Sales")
-        plt.title("Sales by Country")
-        plt.savefig('sales_country.png')
-        plt.close()
-        return "Sales by country visualization created"
-    else:
-        return "Error: No data available for country visualization"
-
-def salesbyyearvisualisation():
-    df = load_clean_data()
-    if isinstance(df, pd.DataFrame):
-        plt.figure()
-        df.groupby("YEAR_ID")["SALES"].sum().plot(kind='line')
-        plt.xlabel("Year")
-        plt.ylabel("Sales")
-        plt.title("Sales by Year")
-        plt.savefig('sales_year.png')
-        plt.close()
-        return "Sales by year visualization created"
-    else:
-        return "Error: No data available for year visualization"
 
