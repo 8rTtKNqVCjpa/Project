838bd14092a8275bf8402957d9af23ae141ce391
Dinalys <dinada2003@gmail.com>
2024-07-30 15:28:16 +0100

    feat: ajout des instances des applications dash pour chaque visualisation

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 from app import routes
+from app.dash import apl
+from app.dashheatmap import apl
+from app.dashgraph import apl
+from app.dashbar import apl
